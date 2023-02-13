a0d43b74725a52815df7fcd5fc13c1cab54a4e9b
Shah Hafiz <shahhafiz@appsploration.com>
2023-02-13 12:36:47 +0800

    Fix cards not hidden

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+// hidden -> visible -> seen -> resee -> hiddden
+
 const cardGroups = document.querySelectorAll('.card-group')
+const allCards = document.querySelectorAll('.cards')
+const adFormat = document.querySelector('.ad-format')
 const cards1 = cardGroups[0].querySelectorAll('.card')
 const cards2 = cardGroups[1].querySelectorAll('.card')
 const cards3 = cardGroups[2].querySelectorAll('.card')
@@ -7,6 +11,10 @@ const cards3 = cardGroups[2].querySelectorAll('.card')
 const threshold = 300;
 const threshold2 = 600;
 const threshold3 = 900;
+
+allCards.forEach(card => {
+    setCardHidden(card)
+})
 
 let wasShowing = 0
 addEventListener("scroll", (event) => {
@@ -17,66 +25,80 @@ addEventListener("scroll", (event) => {
         cards1.forEach(card => {
             card.classList.remove('visible')
             card.classList.remove('resee')
-            card.classList.add('hidden')
+            setCardHidden(card)
         })
         wasShowing = 0
     }
     else if(scrollOffset >= threshold && scrollOffset < threshold2){
         console.log('show 1')
-        if(wasShowing == 0){
-            cards1.forEach(card => {
-                card.classList.remove('hidden')
-                card.classList.add('visible')
-            })
-        } else if (wasShowing == 2){
-            cards2.forEach(card => {
-                card.classList.remove('visible')
-                card.classList.remove('resee')
-                card.classList.add('hidden')
-            })
-            cards1.forEach(card => {
-                card.classList.remove('seen')
-                card.classList.add('resee')
-            })
-        }
-        wasShowing = 1
+        cards1.forEach(card => {
+            if(card.classList.contains('hidden')){
+                setCardVisible(card)
+            }
+
+            if(card.classList.contains('seen')){
+                setCardResee(card)
+            }
+        })
+
+        cards2.forEach(card => {
+            setCardHidden(card)
+        })
+        
     } else if(scrollOffset >= threshold2 && scrollOffset < threshold3){
         console.log('show 2')
-        if(wasShowing == 1){
-            cards1.forEach(card => {
-                card.classList.remove('visible');
-                card.classList.remove('resee');
-                card.classList.add('seen');
-            })
-            cards2.forEach(card => {
-                card.classList.remove('hidden');
-                card.classList.add('visible');
-            })
-        } else if (wasShowing == 3){
-            cards3.forEach(card => {
-                card.classList.remove('visible')
-                card.classList.add('hidden')
-            })
-            cards2.forEach(card => {
-                card.classList.remove('seen')
-                card.classList.add('resee')
-            })
-        }
-        wasShowing = 2
+        cards1.forEach(card => {
+            setCardSeen(card)
+            
+        })
+
+        cards2.forEach(card => {
+            if(card.classList.contains('hidden')){
+                setCardVisible(card)
+            }
+
+            if(card.classList.contains('seen')){
+                setCardResee(card)
+            }
+        })
+
+        cards3.forEach(card => {
+            setCardHidden(card)
+        })
     } else if(scrollOffset >= threshold3){
         console.log('show 3')
-        if(wasShowing == 2){
-            cards2.forEach(card => {
-                card.classList.remove('visible');
-                card.classList.remove('resee');
-                card.classList.add('seen');
-            })
-            cards3.forEach(card => {
-                card.classList.remove('hidden');
-                card.classList.add('visible');
-            })
-        }
+        
+        cards2.forEach(card => {
+            setCardSeen(card)
+        })
 
-        wasShowing = 3
+        cards3.forEach(card => {
+            setCardVisible(card)
+        })
     }
 })
+
+function setCardHidden(card){
+    card.classList.remove('visible')
+    card.classList.remove('resee')
+    card.classList.add('hidden');
+    card.style.translate = `0 ${adFormat.clientHeight + 100}px`;
+}
+
+function setCardVisible(card){
+    card.classList.remove('hidden');
+    card.classList.remove('hidden');
+    card.classList.add('visible')
+}
+
+function setCardSeen(card){
+    card.style.translate = `0 ${adFormat.clientHeight + 100}px`;
+    card.classList.remove('visible')
+    card.classList.add('seen');
+}
+
+function setCardResee(card){
+    card.style = ''
+    card.classList.remove('seen')
+    card.classList.add('resee')
+}
