f9fec080b6c89452dcab0ce5184562abb7fb7daa
Shah Hafiz <shahhafiz@appsploration.com>
2023-02-13 09:42:07 +0800

    Add animation after card has been seen and when card is resee

--- app.js
+++ app.js
@@ -5,46 +5,78 @@ const cards3 = cardGroups[2].querySelectorAll('.card')
 
 
 const threshold = 300;
-const threshold2 = 800;
-const threshold3 = 1300;
+const threshold2 = 600;
+const threshold3 = 900;
 
+let wasShowing = 0
 addEventListener("scroll", (event) => {
     const scrollOffset = window.pageYOffset
     console.log(scrollOffset)
 
-    if(scrollOffset > threshold && scrollOffset < threshold2){
+    if(scrollOffset < threshold){
         cards1.forEach(card => {
-            card.classList.add('visible')
-            card.classList.remove('hidden')
+            card.classList.remove('visible')
+            card.classList.remove('resee')
+            card.classList.add('hidden')
         })
-    } else {
-        cards1.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.add('hidden');
-        })
+        wasShowing = 0
     }
+    else if(scrollOffset >= threshold && scrollOffset < threshold2){
+        console.log('show 1')
+        if(wasShowing == 0){
+            cards1.forEach(card => {
+                card.classList.remove('hidden')
+                card.classList.add('visible')
+            })
+        } else if (wasShowing == 2){
+            cards2.forEach(card => {
+                card.classList.remove('visible')
+                card.classList.remove('resee')
+                card.classList.add('hidden')
+            })
+            cards1.forEach(card => {
+                card.classList.remove('seen')
+                card.classList.add('resee')
+            })
+        }
+        wasShowing = 1
+    } else if(scrollOffset >= threshold2 && scrollOffset < threshold3){
+        console.log('show 2')
+        if(wasShowing == 1){
+            cards1.forEach(card => {
+                card.classList.remove('visible');
+                card.classList.remove('resee');
+                card.classList.add('seen');
+            })
+            cards2.forEach(card => {
+                card.classList.remove('hidden');
+                card.classList.add('visible');
+            })
+        } else if (wasShowing == 3){
+            cards3.forEach(card => {
+                card.classList.remove('visible')
+                card.classList.add('hidden')
+            })
+            cards2.forEach(card => {
+                card.classList.remove('seen')
+                card.classList.add('resee')
+            })
+        }
+        wasShowing = 2
+    } else if(scrollOffset >= threshold3){
+        console.log('show 3')
+        if(wasShowing == 2){
+            cards2.forEach(card => {
+                card.classList.remove('visible');
+                card.classList.remove('resee');
+                card.classList.add('seen');
+            })
+            cards3.forEach(card => {
+                card.classList.remove('hidden');
+                card.classList.add('visible');
+            })
+        }
 
-    if(scrollOffset > threshold2  && scrollOffset < threshold3){
-        cards2.forEach(card => {
-            card.classList.add('visible')
-            card.classList.remove('hidden')
-        })
-    } else {
-        cards2.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.add('hidden');
-        })
-    }
-
-    if(scrollOffset > threshold3){
-        cards3.forEach(card => {
-            card.classList.add('visible')
-            card.classList.remove('hidden')
-        })
-    } else {
-        cards3.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.add('hidden');
-        })
+        wasShowing = 3
     }
 })
